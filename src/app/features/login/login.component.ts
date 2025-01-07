6639ff407f3cae54cb83ed0712b1be39185d764c
Marianela Landoni <mlandonih@hotmail.com>
2025-01-07 14:05:09 +0100

    feat: add login

---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputComponent } from '../../shared/input/input.component';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, InputComponent, ButtonComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(){
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+     this.router.navigate(['/success']);
+    }
+  }
+
+  get emailControl(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get passwordControl(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
+}
