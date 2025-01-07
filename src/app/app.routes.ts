30a00d9d3b5802112cab72a88caa7e006238f851
Marianela Landoni <mlandonih@hotmail.com>
2025-01-07 14:05:32 +0100

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'success',
+    loadComponent: () => import('./features/success/success.component').then(m=>m.SuccessComponent)
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+
+];
